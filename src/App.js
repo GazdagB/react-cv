fcb69e20a60db1827821eca8e777cbbb3fe70e97
The Code Designer <gazdagbal@gmail.com>
2023-07-17 16:39:52 +0200

    github deploy test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
-import logo from './logo.svg';
 import './App.css';
+//Dependencies Imports
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+
+//Image Imports
+import logo from './images/theCodeDesigner.png'
+
+//Font Awesome Imports
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faInstagram, faSquareInstagram, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+// Librarry for Font Awesome
+library.add(faGithub,faSquareInstagram,faTwitter,faYoutube);
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <Router>
+      {/*Header*/}
+      <header>
+        <nav className='navbar'>
+
+          <Link to={"/"}>
+          <img className="navbar__logo" src={logo} alt=""/>
+          </Link>
+            
+
+        {/* Nav Links */}
+        <ul className='navbar__list'>
+
+            <li>
+              <Link className='navbar__list__item' to={"/"} >
+              Home
+              </Link>
+            </li>
+
+            <li>
+              <Link className='navbar__list__item' to={"/about"} >
+                About
+              </Link>
+            </li>
+
+            <li>
+              <Link className='navbar__list__item' to={"/work"} >
+                Work
+                </Link>
+            </li>
+
+            <li>
+              <Link 
+              className='navbar__list__item' to={"/contact"} >
+                Contact
+              </Link>
+            </li>
+
+        </ul>
+        
+        {/* Socail Links  */}
+        <div className="navbar__socials">
+        <a href="https://github.com/theCodeDesigner" target='_blank'>
+          <FontAwesomeIcon className='navbar__socials__icon' icon={faGithub} />
         </a>
+
+        <a href="https://instagram.com/thecode_designer?igshid=OGQ5ZDc2ODk2ZA==" target='_blank'>
+          <FontAwesomeIcon className='navbar__socials__icon' icon={faSquareInstagram} />
+        </a>
+
+        <a href="https://twitter.com/CodeDesignr" target='_blank'>
+          <FontAwesomeIcon className='navbar__socials__icon' icon={faTwitter} />
+        </a>
+
+        <a href="https://youtube.com/@TheCodeDesigner" target='_blank'>
+          <FontAwesomeIcon className='navbar__socials__icon' icon={faYoutube} />
+        </a>
+        
+        </div>
+
+        </nav>
       </header>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <footer>
+      <p>© 2023 The Code Designer</p>
+      </footer>
+      {/* Routes */}
+      
+    </Router>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import memoji from '../images/memoji.png'
+import './Home.css'
+import { Link } from 'react-router-dom'
+
+//font awesome imports
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+//Free solid icons 
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <div className='home__left'>
+        <h1 className='home__left__text'>
+            <span className="home__greet">Hi There</span> 👋🏻
+            <br />
+            I'm Balázs <span className="home__name">Gazdag!</span>
+            <br />
+             <span className="the-code-designer">&lt; theCodeDesigner /&gt;</span>
+        </h1>
+        <h2>
+            I'm a <span className="home__title">Frontend Developer</span> & <span className="home__title">Graphic Designer</span>
+        </h2>
+        <div className="home__left__cta">
+            <button className='home__left__button'>About me..</button>
+            <Link className='home__left__cta__link' to={"./work"}>See My Work <FontAwesomeIcon icon={faArrowUp} className='faArrowUp' /></Link>
+        </div>
+        </div>
+        <div className="home__right">
+            <img className='home__right__image' src={memoji} alt=""/>
+        </div>
+    </div>
+  )
+}
+
+export default Home
