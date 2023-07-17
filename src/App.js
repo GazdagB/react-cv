18d5bb48f03cb6c246a24598f7367d441ed787ec
The Code Designer <gazdagbal@gmail.com>
2023-07-17 17:17:40 +0200

    Added dots for relative urls

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
       <header>
         <nav className='navbar'>
 
-          <Link to={"/"}>
+          <Link to={"./"}>
           <img className="navbar__logo" src={logo} alt=""/>
           </Link>
             
@@ -33,26 +33,26 @@ function App() {
         <ul className='navbar__list'>
 
             <li>
-              <Link className='navbar__list__item' to={"/"} >
+              <Link className='navbar__list__item' to={"./"} >
               Home
               </Link>
             </li>
 
             <li>
-              <Link className='navbar__list__item' to={"/about"} >
+              <Link className='navbar__list__item' to={"./about"} >
                 About
               </Link>
             </li>
 
             <li>
-              <Link className='navbar__list__item' to={"/work"} >
+              <Link className='navbar__list__item' to={"./work"} >
                 Work
                 </Link>
             </li>
 
             <li>
               <Link 
-              className='navbar__list__item' to={"/contact"} >
+              className='navbar__list__item' to={"./contact"} >
                 Contact
               </Link>
             </li>
